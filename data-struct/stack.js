f359fe9c62c7829af3f180dbd1b98e3acba9d801
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-04-21 09:58:46 -0300

    implement push stack function

--- data-struct/stack.js
+++ data-struct/stack.js
@@ -1,2 +1,13 @@
-const elements = []
-const topo = -1
+let elements = []
+let topo     = -1
+const MAX      = 10
+
+function push(number){
+    if( topo < MAX ){
+        topo = topo + 1
+        elements[topo] = number
+    }
+    else{
+        console.log("A pilha está cheia!")
+    }
+}
