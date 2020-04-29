f94df07d03e877463fc1c7f158e2771825f00c73
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-04-28 23:46:42 -0300

    create cpfValidation and validation function in script.js

--- javascript/gama-no-javascript-basico/static/js/script.js
+++ javascript/gama-no-javascript-basico/static/js/script.js
@@ -1,5 +1,19 @@
 console.log("Java script carregado")
 
+function validation(cpf){
+    return false
+}
+
 function cpfValidation(){
     console.log("Iniciando a validação do CPF!")
+    let cpf = document.getElementById("cpf_input").value
+    
+    let validationResult = validation(cpf)
+
+    if (validationResult) {
+        document.getElementById("success").style.display='block'
+    }
+    else{
+        document.getElementById("error").style.display='block'
+    }
 }
