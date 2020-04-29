37574e03763e35ec4928aaade1458cb62c61a1ca
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-04-29 11:34:55 -0300

    clean output in browser with .style.display='none'

--- javascript/gama-no-javascript-basico/static/js/script.js
+++ javascript/gama-no-javascript-basico/static/js/script.js
@@ -1,9 +1,11 @@
 console.log("Java script carregado")
 
 function validation(cpf){
-    if(cpf.length != 11){
+
+    if(cpf.length != 11) {
         return false
-    }else{
+    }
+    else {
         let numbers = cpf.substring(0, 9)
         let digits = cpf.substring(9) 
         
@@ -11,21 +13,38 @@ function validation(cpf){
         for(let i = 10; i > 1; i--){
             sum += numbers.charAt(10 - i) * i;
         }
-        console.log(sum)
 
-        let result = sum % 11 < 2 ? 0 : 11 - ( sum % 11 )
+        let result_1 = sum % 11 < 2 ? 0 : 11 - ( sum % 11 )
 
-        if(result != digits.charAt(0))
+        //first digit validation
+        if(result_1 != digits.charAt(0)){
             return false
+        }
+
+        sum = 0
+        numbers = cpf.substring(0, 10)
+
+        for(let k = 11; k > 1; k--){
+            sum += numbers.charAt(11 - k) * k
+        }
+
+        let result_2 = sum % 11 < 2 ? 0 : 11 - ( sum % 11 )
+
+        //second digit validation
+        if(result_2 != digits.charAt(1)){
+            return false
+        }
+
+        return true
     }
-
-    console.log(digits.toString().charAt(0) + "é a primeira posição da variável sum")
-
-    return true
 }
 
 function cpfValidation(){
+    
     console.log("Iniciando a validação do CPF!")
+    document.getElementById('success').style.display='none'
+    document.getElementById('error').style.display='none'
+
     let cpf = document.getElementById("cpf_input").value
     
     let validationResult = validation(cpf)
