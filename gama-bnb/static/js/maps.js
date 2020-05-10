68cef5f1706d1edc624a90f15a197b00543b0002
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-05-10 14:09:56 -0300

    create footer

---
+++ gama-bnb/static/js/maps.js
@@ -0,0 +1,34 @@
+var initialCoordinates = [-22.91, -43.20]; // Rio de Janeiro
+var initialZoomLevel = 13;
+
+// create a map in the "map" div, set the view to a given place and zoom
+var map = L.map('map').setView(initialCoordinates, initialZoomLevel);
+
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; Contribuidores do <a href="http://osm.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var muxiCoordinates = [-22.903719, -43.1760605];
+var muxiMarkerMessage = "A Muxi fica aqui.<br>Olá mundo!";
+
+L.marker(muxiCoordinates).addTo(map)
+    .bindPopup(muxiMarkerMessage)
+    .openPopup();
+
+var muxiCoordinates = [-22.903719, -43.1760605];
+var muxiMarkerMessage = "A Muxi fica aqui.<br>Olá mundo!";
+
+var muxiIconProperties = {
+  iconUrl: "http://muxi.com.br/app/webroot/img/maps-marker.png"
+, iconSize: [44, 59]
+, iconAnchor: [22, 59]
+, popupAnchor: [0, -50]
+};
+
+var muxiIcon = L.icon(muxiIconProperties);
+
+L.marker(muxiCoordinates, {icon: muxiIcon})
+  .addTo(map)
+  .bindPopup(muxiMarkerMessage)
+;
