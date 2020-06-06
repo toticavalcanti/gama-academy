7e18f95f8397c9f32bf45542d7820afc8e8c1760
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-05 21:18:11 -0300

    listar

--- node/10-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ node/10-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -3,12 +3,28 @@ const Postgres = require('./../db/strategies/postgres')
 const Context = require('./../db/strategies/base/contextStrategy')
 
 const context = new Context(new Postgres())
+const MOCK_HEROI_CADASTRAR = { nome: 'Gaviao Negro', poder: 'flexas'}
+
+
 
 describe('Postgres Strategy', function(){
     this.timeout(Infinity)
+    this.beforeAll(async function(){
+        context.connect()
+    })
 
     it('PostgresSQL Connection', async function () {
         const result = await context.isConnected()
         assert.equal(result, true)
+    })
+    it('cadastrar', async function(){
+        const result = await context.create(MOCK_HEROI_CADASTRAR)
+        delete result.id
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
+    })
+    it('listar', async function(){
+        const [result] = await context.read({ nome:MOCK_HEROI_CADASTRAR.nome })
+        delete result.id
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
     })
 })
