6887f7cad5f9c33492aefd60d83761f223efefb9
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-09 17:43:59 -0300

    mongodb

---
+++ node/11-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
@@ -0,0 +1,25 @@
+const ICrud = require('./interfaces/interfaceCrud')
+
+class MongoDB extends ICrud {
+    constructor(){
+        super()
+    }
+
+    defineModel() {
+        
+    }
+
+    isConnected() {
+
+    }
+
+    connect() {
+
+    }
+
+    create(item){
+        console.log('O item foi salvo no MongoDB')
+    }
+}
+
+module.exports = MongoDB 
