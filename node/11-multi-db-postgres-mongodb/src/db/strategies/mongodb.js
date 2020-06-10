8e0f461ab8577867f734ab0cdad72f17989ad584
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-10 09:34:26 -0300

    mongo update method in db/strategies/mongodb.js

--- node/11-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
+++ node/11-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
@@ -65,6 +65,14 @@ class MongoDB extends ICrud {
     create(item) {
         return this._herois.create(item)
     }
+
+    read(item, skip = 0, limit = 10) { 
+        return this._herois.find(item).skip(skip).limit(limit)
+    }
+
+    update(id, item) {
+        return this._herois.updateOne({_id: id}, {$set: item})
+    }
 }
 
 module.exports = MongoDB 
--- node/11-multi-db-postgres-mongodb/src/tests/mongdbStrategy.test.js
+++ node/11-multi-db-postgres-mongodb/src/tests/mongdbStrategy.test.js
@@ -7,10 +7,25 @@ const MOCK_HEROI_CADASTRAR = {
     poder: 'laço'
 }
 
+const MOCK_HEROI_DEFAULT= {
+    nome: `Homem Aranha${Date.now()}`,
+    poder: 'Super teia'
+}
+
+const MOCK_HEROI_ATUALIZAR= {
+    nome: `Patolino${Date.now()}`,
+    poder: 'Velocidade'
+}
+
+let MOCK_HEROI_ID = ''
+
 const context = new Context(new MongoDB())
 describe('MongoDB Suite de Testes', function() {
     this.beforeAll(async () => {
         await context.connect()
+        await context.create(MOCK_HEROI_DEFAULT)
+        const result =await context.create(MOCK_HEROI_ATUALIZAR)
+        MOCK_HEROI_ID = result._id
     })
 
     it('verificar conexao', async () => {
@@ -22,5 +37,22 @@ describe('MongoDB Suite de Testes', function() {
     it('cadastrar', async () => {
         const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR)
         assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR)
+    })
+
+    it('listar', async () => {
+        const [{nome, poder}] = await context.read({nome: MOCK_HEROI_DEFAULT.nome}).limit(2)
+        const result = {
+            nome, poder
+        }
+        assert.deepEqual(result, MOCK_HEROI_DEFAULT)
+    })
+
+    it('atualizar', async () => {
+        console.log(MOCK_HEROI_ID)
+        const result = await context.update(MOCK_HEROI_ID, {
+            nome: 'Pernalonga',
+        })
+        console.log(result)
+        assert.deepEqual(result.nModified, 1)
     })
 })
