5e4e40dd26f2af209b7a3349668ba25b553958e8
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-14 14:50:20 -0300

    start hapi-user-validation

---
+++ node/23-hapi-user-validation/src/tests/authHeroes.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+const api = require('../api')
+let app = {}
+
+describe('Auth test suite', function () {
+    this.beforeAll( async () => {
+        app = await api
+    })
+
+    it('deve obter um token', async () => {
+        const result = await app.inject ({
+            method: 'POST',
+            url: '/login',
+            payload: {
+                username: 'Olívia Palito',
+                password: '123'
+            }
+        })
+        const statusCode = result.statusCode
+        const dados = JSON.parse(result.payload)
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados.token.length > 10)
+    })
+})
