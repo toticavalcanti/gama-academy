34c0eeb99b233957885cf90321e5341014eb4dd7
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-24 17:03:08 -0300

    graphQL - async request

--- GraphQL/client-server-example/packages/web/src/pages/Sign-in.js
+++ GraphQL/client-server-example/packages/web/src/pages/Sign-in.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('https://127.0.0.1:8000/authenticate', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        console.log('Sucess!');
+      })
+  };
+
+  const handleEmailChange = (event) => setEmail(event.target.value)
+  const handlePasswordChange = (event) => setPassword(event.target.value)
   return (
-    <form action="/authenticate" method="POST">
+    <form onSubmit={handleSubmit} >
     <fieldset>
-      <label for="email">E-mail</label>
-      <input id="email" name="email" type="email" inputmode="email" autocomplete="username" />
+      <label htmlFor="email">E-mail</label>
+      <input 
+        id="email" 
+        type="email" 
+        value={email}
+        onChange={handleEmailChange}
+        inputMode="email" 
+        autoComplete="username" 
+      />
     </fieldset>
     <fieldset>
-      <label for="password">Password</label>
-      <input id="password" name="password" type="password" autocomplete="current-password" />
+      <label htmlFor="password">Password</label>
+      <input 
+        id="password" 
+        type="password" 
+        autoComplete="current-password" 
+        value={password}
+        onChange={handlePasswordChange}
+      />
     </fieldset>
     <button type="submit">Enter</button>
   </form>
