7e58b704c72c7f9b74fb3153a2bdb67e91a610f5
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-24 15:09:27 -0300

    package with reactjs

--- GraphQL/client-server-example/packages/server/src/main.js
+++ GraphQL/client-server-example/packages/server/src/main.js
@@ -1,6 +1,4 @@
 import { createServer } from 'http';
-import { readFile } from 'fs';
-import { resolve } from 'path';
 import { parse } from 'querystring';
 
 const server = createServer((request, response) => {
@@ -19,37 +17,6 @@ const server = createServer((request, response) => {
       break;
     }
 
-    case '/sign-in': {
-      const path = resolve(__dirname, '../../web/sign-in.html');
-      readFile(path, (error, file) => {
-        if (error) {
-          response.writeHead(500, 'Cannot process HTML file.');
-          response.end();
-          return;
-        }
-        response.writeHead(200);
-        response.write(file);
-        response.end();
-      });
-      break;
-    }
-    
-    case '/home': {
-      const path = resolve(__dirname, './pages/home.html');
-      readFile(path, (error, file) => {
-        if(error){
-          response.writeHead(500, 'Cannot process HTML file.');
-          response.end();
-          return;
-        }
-        response.writeHead(200);
-        response.write(file);
-        response.end();
-      });
-      break;
-
-    }
-
     case '/authenticate': {
       let data = '';
       request.on('data', (chunk) => {
@@ -57,9 +24,7 @@ const server = createServer((request, response) => {
       });
       request.on('end', () => {
         const params = parse(data);
-        response.writeHead(301, {
-          Location: '/home',
-        }); 
+        
         response.end();
       });
       break;
