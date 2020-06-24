1b033e168ae090b0289218f1e5e299b371ec481d
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-24 13:04:51 -0300

    graphQL - rendering, params and redirections

--- GraphQL/client-server-example/src/main.js
+++ GraphQL/client-server-example/src/main.js
@@ -1,17 +1,70 @@
 import { createServer } from 'http';
+import { readFile } from 'fs';
+import { resolve } from 'path';
+import { parse } from 'querystring';
 
 const server = createServer((request, response) => {
-  switch(request.url){
-    case'/status': {
+  switch(request.url) {
+    
+    case '/status': {
       response.writeHead(200, {
         'Content-Type': 'application/json',
       });
-      response.write(JSON.stringify({
-        status: 'Okay!',
-      }));
+      response.write(
+        JSON.stringify({
+          status: 'Okay!',
+        })
+      );
       response.end();
       break;
     }
+
+    case '/sign-in': {
+      const path = resolve(__dirname, './pages/sign-in.html');
+      readFile(path, (error, file) => {
+        if (error) {
+          response.writeHead(500, 'Cannot process HTML file.');
+          response.end();
+          return;
+        }
+        response.writeHead(200);
+        response.write(file);
+        response.end();
+      });
+      break;
+    }
+    
+    case '/home': {
+      const path = resolve(__dirname, './pages/home.html');
+      readFile(path, (error, file) => {
+        if(error){
+          response.writeHead(500, 'Cannot process HTML file.');
+          response.end();
+          return;
+        }
+        response.writeHead(200);
+        response.write(file);
+        response.end();
+      });
+      break;
+
+    }
+
+    case '/authenticate': {
+      let data = '';
+      request.on('data', (chunk) => {
+        data += chunk;
+      });
+      request.on('end', () => {
+        const params = parse(data);
+        response.writeHead(301, {
+          Location: '/home',
+        }); 
+        response.end();
+      });
+      break;
+    }
+
     default: {
       response.writeHead(404, 'Server not found.');
       response.end();
