4f273c4abf4bf8628ce854cffc29ba9fb23f7f1d
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-25 14:38:17 -0300

    graphQL - typeDefs and resolvers

--- GraphQL/client-server-example/packages/server/src/main.js
+++ GraphQL/client-server-example/packages/server/src/main.js
@@ -1,57 +1,22 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
 
 const app = express();
 
 const server = new ApolloServer({
-  typeDefs: gql`
-  type Client {
-      id: ID!
-      name: String!
-    }
-
-    type Demand{
-      id: ID!
-      name: String!
-      client: Client!
-      deadLine: String
-    }
-
-    type Query {
-      demands: [Demand]!
-    }
-  `,
-
-  resolvers: {
-    Query: {
-      demands: () => [],
-    }
-  }
+  typeDefs,
+  resolvers,
 });
 
 server.applyMiddleware({
   app,
   cors: {
     origin: 'http://localhost:3000',
-  }
-})
-
-// server.get('/status', (_, response) => {
-//   response.send({
-//     status: 'Okay!',
-//   });
-// });
-
-
-
-// server
-//   .options('/authenticate', enableCors)
-//   .post('/authenticate', enableCors, express.json(), (request, response) => {
-//   console.log('Email: ', request.body.email, 'Password: ', request.body.password);
-//   response.send({
-//     Okay: true, 
-//   });
-// });
+  },
+  bodyParserConfig: true,
+});
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.PORT || '127.0.0.1';
