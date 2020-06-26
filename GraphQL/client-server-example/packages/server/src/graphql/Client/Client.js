9b1167ae483d55e5f003915078e3bd0375ac77db
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-26 11:52:51 -0300

    graphQL - pagination, order and filters

--- GraphQL/client-server-example/packages/server/src/graphql/Client/Client.js
+++ GraphQL/client-server-example/packages/server/src/graphql/Client/Client.js
@@ -1,19 +1,39 @@
 import { gql } from 'apollo-server-express';
 import createRepository from '../../io/Database/createRepository';
+import { ListSortmentEnum } from '../List/List';
 
 const clientRepository = createRepository('client'); 
 
 export const typeDefs = gql`
-  type Client {
+  type Client implements Node {
     id: ID!
     name: String!
     email: String!
     disabled: Boolean!
   }
 
+  type ClientList implements List {
+    items: [Client!]!
+    totalItems: Int!
+  }
+
+  input ClientListFilter {
+    name: String
+    email: String
+    disabled: Boolean
+  }
+
+  input ClientListOptions {
+    take: Int
+    skip: Int
+    sort: ListSort
+    filter: ClientListFilter
+  }
+  
+
   extend type Query {
     client(id: ID!): Client
-    clients: [Client!]!
+    clients(options: ClientListOptions): ClientList
   }
 `;
 
@@ -27,9 +47,59 @@ export const resolvers = {
       return clients.find((client) => client.id == id );
      },
 
-    clients: async () => {
-        const clients = await clientRepository.read();
-      return clients; 
-     },
+    clients: async (_, args) => {
+      const { take = 10, skip = 0, sort, filter} = args.options || {};
+
+      /**
+       * @type {Array.<*>}
+       */
+      const clients = await clientRepository.read();
+
+      if (sort) {
+        clients.sort((clientA, clientB) => {
+          if (!['name', 'email', 'disabled'].includes(sort.sorter))
+            throw new Error(`Cannot sort by field "${sort.sorter}".`);
+
+          const fieldA = clientA[sort.sorter];
+          const fieldB = clientB[sort.sorter];
+
+          if (typeof fieldA === 'string') {
+            if (sort.sortment === ListSortmentEnum.ASC)
+              return fieldA.localeCompare(fieldB);
+            else return fieldB.localeCompare(fieldA);
+          }
+
+          if (sort.sortment === ListSortmentEnum.ASC)
+            return Number(fieldA) - Number(fieldB);
+          else return Number(fieldB) - Number(fieldA);
+        });
+      }
+
+      const filteredClients = clients.filter((client) => {
+        if (!filter || Object.keys(filter).length === 0) return true;
+
+        return Object.entries(filter).every(([field, value]) => {
+          if (client[field] === null || client[field] === undefined)
+            return false;
+          if (typeof value === 'string') {
+            if (value.startsWith('%') && value.endsWith('%'))
+              return client[field].includes(value.substr(1, value.length - 2));
+            if (value.startsWith('%'))
+              return client[field].endsWith(value.substr(1));
+            if (value.endsWith('%'))
+              return client[field].startsWith(
+                value.substr(0, value.length - 1)
+              );
+            return client[field] === value;
+          }
+          return client[field] === value;
+        });
+      });
+
+      return {
+        items: filteredClients.slice(skip, skip + take),
+        totalItems: filteredClients.length,
+      };
+    },
   },
-};
+}
--- GraphQL/client-server-example/packages/server/src/graphql/typeDefs.js
+++ GraphQL/client-server-example/packages/server/src/graphql/typeDefs.js
@@ -1,6 +1,7 @@
 import { gql } from 'apollo-server-express';
 
 import { typeDefs as nodeTypeDefs } from './Node/Node';
+import { typeDefs as listTypeDefs } from './List/List';
 import { typeDefs as clientTypeDefs } from './Client/Client';
 import { typeDefs as demandTypeDefs } from './Demand/Demand';
 
@@ -14,6 +15,7 @@ const typeDefs = gql`
   }
 
   ${nodeTypeDefs}
+  ${listTypeDefs}
   ${clientTypeDefs}
   ${demandTypeDefs}
 `;
